315f35afc9dc42b1b85c1f7e68736a12059e2c46
Blank <blank.xu@outlook.com>
2024-11-13 17:23:22 +0100

    docs: add examples for sqlx, xorm and gorm

---
+++ examples/sqlx/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	sqladapter "github.com/Blank-Xu/sql-adapter"
+	"github.com/casbin/casbin/v2"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	driverName = "mysql"
+	tableName  = "casbin_rule_example"
+
+	envFile = "../../test/.env"
+
+	rbacModelFile = "../../test/testdata/rbac_model.conf"
+)
+
+var dataSource = func() string {
+	envMap := loadEnvfile(envFile)
+
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",
+		envMap["TEST_DATABASE_USER"],
+		envMap["TEST_DATABASE_PASSWORD"],
+		envMap["TEST_DATABASE_HOST"],
+		envMap["TEST_DATABASE_PORT_MYSQL"],
+		envMap["TEST_DATABASE_NAME"])
+}()
+
+func main() {
+	// connect to the database first.
+	dbx, err := sqlx.Open(driverName, dataSource)
+	if err != nil {
+		panic(err)
+	}
+	if err = dbx.Ping(); err != nil {
+		panic(err)
+	}
+	defer dbx.Close()
+
+	dbx.SetMaxOpenConns(20)
+	dbx.SetMaxIdleConns(10)
+	dbx.SetConnMaxLifetime(time.Minute * 10)
+
+	// Initialize an adapter and use it in a Casbin enforcer:
+	// The adapter will use the MySQL table name "casbin_rule_example",
+	// the default table name is "casbin_rule".
+	// If it doesn't exist, the adapter will create it automatically.
+	a, err := sqladapter.NewAdapter(dbx.DB, driverName, tableName)
+	if err != nil {
+		panic(err)
+	}
+
+	e, err := casbin.NewEnforcer(rbacModelFile, a)
+	if err != nil {
+		panic(err)
+	}
+
+	// Load the policies from DB.
+	if err = e.LoadPolicy(); err != nil {
+		log.Println("LoadPolicy failed, err: ", err)
+	}
+
+	// Check the permission.
+	has, err := e.Enforce("alice", "data1", "read")
+	if err != nil {
+		log.Println("Enforce failed, err: ", err)
+	}
+	if !has {
+		log.Println("do not have permission")
+	}
+
+	// Modify the policy.
+	// e.AddPolicy(...)
+	// e.RemovePolicy(...)
+
+	// Save the policy back to DB.
+	if err = e.SavePolicy(); err != nil {
+		log.Println("SavePolicy failed, err: ", err)
+	}
+}
+
+func loadEnvfile(envFile string) map[string]string {
+	f, err := os.Open(envFile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	m := make(map[string]string)
+	for scanner.Scan() {
+		text := strings.TrimSpace(scanner.Text())
+		if text == "" || text[0] == '#' {
+			continue
+		}
+
+		s := strings.Split(text, "=")
+		if len(s) < 2 {
+			panic("invalid env file: " + envFile)
+		}
+
+		m[s[0]] = strings.Join(s[1:], "")
+	}
+
+	if err = scanner.Err(); err != nil {
+		panic("load env file failed, err: " + err.Error())
+	}
+
+	return m
+}
---
+++ examples/xorm/go.mod
@@ -0,0 +1,25 @@
+module xorm-example
+
+go 1.21
+
+replace github.com/Blank-Xu/sql-adapter => ../../.
+
+require (
+	github.com/Blank-Xu/sql-adapter v1.0.4
+	github.com/casbin/casbin/v2 v2.100.0
+	github.com/go-sql-driver/mysql v1.8.1
+	xorm.io/xorm v1.3.9
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/bmatcuk/doublestar/v4 v4.7.1 // indirect
+	github.com/casbin/govaluate v1.2.0 // indirect
+	github.com/goccy/go-json v0.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
+)
